0d402daa9b033085cf0e8506a9e30919c1f49d5d
Jkarage <josephbkarage@gmail.com>
2022-12-15 10:51:44 +0300

    Removed getting userid explicitly

--- utils/shortener.go
+++ utils/shortener.go
@@ -2,13 +2,12 @@ package utils
 
 import (
 	"crypto/sha256"
-	"errors"
 	"fmt"
 	"math/big"
 	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/itchyny/base58-go"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func sha256Of(input string) []byte {
@@ -27,14 +26,11 @@ func base58Encoded(bytes []byte) string {
 	return string(encoded)
 }
 
-func GenerateShortLink(initialLink string, userid primitive.ObjectID) (string, error) {
-	if primitive.IsValidObjectID(userid.Hex()) {
-		initialLink += userid.Hex()
-		urlHashBytes := sha256Of(initialLink)
-		generatedNumber := new(big.Int).SetBytes(urlHashBytes).Uint64()
-		finalString := base58Encoded([]byte(fmt.Sprintf("%d", generatedNumber)))
-		return finalString[:9], nil
-	} else {
-		return "", errors.New("the userId used to create the shortened link is invalid")
-	}
+func GenerateShortLink(initialLink string, c *gin.Context) (string, error) {
+	user := TokenDecoder(c)
+	initialLink += user.ID.Hex()
+	urlHashBytes := sha256Of(initialLink)
+	generatedNumber := new(big.Int).SetBytes(urlHashBytes).Uint64()
+	finalString := base58Encoded([]byte(fmt.Sprintf("%d", generatedNumber)))
+	return finalString[:9], nil
 }
