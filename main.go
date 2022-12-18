727c01742595fb420af1d83880d056726c4102aa
Jkarage <josephbkarage@gmail.com>
2022-12-18 16:40:53 +0300

    initialize log

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"github.com/jkarage/alinker/log"
 	"github.com/jkarage/alinker/routers"
 	"github.com/jkarage/alinker/utils"
 )
 
 func main() {
+	var log log.Log
+	log.Initialize()
 	r := routers.InitRoute()
 	utils.InitializeStore()
 	r.Run(":9800")
-
 }
